4080dbbb32f6eafed49c8146b0a1986fffa0292c
pau <a01383351@tec.mx>
2024-03-20 23:23:04 -0600

    next

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Paulina Covarrubias on 20/03/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
