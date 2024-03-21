3514f373416fb1292f4a39b2b2013bc84560f5a1
pau <a01383351@tec.mx>
2024-03-20 22:34:46 -0600

    nuevoooo

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Paulina Covarrubias on 20/03/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView{
+            List(landmarks){
+                landmark in
+                NavigationLink{
+                    LandmarkDetail(landmark: landmark)
+                } label:
+                {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
