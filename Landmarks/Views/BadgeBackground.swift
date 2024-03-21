2150b6042dcaf4a5e9d98ccee2fb3e19f4bb50b5
pau <a01383351@tec.mx>
2024-03-20 23:56:09 -0600

    okay

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,8 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Paulina Covarrubias on 20/03/24.
+//
+
+import Foundation
---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,18 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Paulina Covarrubias on 20/03/24.
+//
+
+import SwiftUI
+
+struct HexagonParameters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HexagonParameters()
+}
